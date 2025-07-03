234d6aec8f31ee13272123f4c4bdf97c87231d77
KrewsUp <156684184+krewsup@users.noreply.github.com>
2025-07-03 16:09:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1399,9 +1399,9 @@ const Footer = ({ onOpenModal }) => {
           <h3>KrewsUp Tech Pvt Ltd</h3>
           <p>Empowering India's gig economy with innovative solutions</p>
           <div className="footer-social">
-            <a href="#" className="twitter"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg></a>
-            <a href="#" className="linkedin"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.067 0-1.14.92-2.066 2.063-2.066 1.14 0 2.066.926 2.066 2.066 0 1.141-.926 2.067-2.066 2.067zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg></a>
-            <a href="#" className="instagram"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.703-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.072-1.689.072-4.948 0-3.259-.013-3.67-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg></a>
+            <a href="https://x.com/RaghavXKrewsUp?t=WSAwWeUH9sPIFO3Uz7Ck5g&s=09" className="twitter"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg></a>
+            <a href="https://www.linkedin.com/in/raghav1548" className="linkedin"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.067 0-1.14.92-2.066 2.063-2.066 1.14 0 2.066.926 2.066 2.066 0 1.141-.926 2.067-2.066 2.067zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg></a>
+            <a href="https://www.instagram.com/raghavxkrewsup?igsh=dHhkOG0yNDNqM2th" className="instagram"><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.703-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.072-1.689.072-4.948 0-3.259-.013-3.67-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg></a>
           </div>
         </div>
         <div className="footer-links-group">
@@ -1479,7 +1479,7 @@ const HomePage = ({ onOpenModal }) => (
         <div className="waitlist-section">
           <h3 className="waitlist-title">The Shift Has Begun</h3>
           <p className="waitlist-subtitle">KrewsUp is reimagining how crews connect.<br/>Get in early. Be part of the new era.</p>
-          <a href="https://forms.gle/your-dummy-link-here" target="_blank" rel="noopener noreferrer" className="waitlist-btn">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSf0AQJNcrFvLjr3FbYqqSPpjN6d9wlRLP2PIZzY0iGt6U3Htg/viewform?usp=header" target="_blank" rel="noopener noreferrer" className="waitlist-btn">
             Join the Waitlist
           </a>
         </div>
@@ -1497,7 +1497,7 @@ const HomePage = ({ onOpenModal }) => (
             </div>
             <div className="testimonial">
                 <p className="testimonial-text">KrewsUp completely changed how we staff our events. In just a few clicks, we connect with reliable talent who are passionate and professional. The same-day payments keep everyone happy and coming back for more gigs.</p>
-                <div className="testimonial-author"><div className="author-avatar">M</div><div><div className="author-name">Mehul Ramaswami</div><div className="author-role">Founder, Kathakonnect Dance Academy, Bangalore</div></div></div>
+                <div className="testimonial-author"><div className="author-avatar">M</div><div><div className="author-name">Mehul Ramaswami</div><div className="author-role">Founder - Kathakonnect Dance Academy, Bangalore</div></div></div>
             </div>
         </div>
     </section>
@@ -1608,12 +1608,16 @@ const CustomersPage = () => {
             </div>
              <div className="testimonials">
                 <div className="testimonial">
-                    <p className="testimonial-text">As a college student, KrewsUp has been a game-changer for me. I can find weekend gigs that fit my schedule, gain real-world experience, and earn money without compromising my studies.</p>
-                    <div className="testimonial-author"><div className="author-avatar">P</div><div><div className="author-name">Priya Sharma</div><div className="author-role">Student & Part-time Event Coordinator</div></div></div>
+                    <p className="testimonial-text">KathaKonnect was electrifying! I helped manage the stage and artist coordination, and it felt like I was part of something big. Thanks to KrewsUp, everything was well-communicated and super smooth. I’d love to do this again!</p>
+                    <div className="testimonial-author"><div className="author-avatar">K</div><div><div className="author-name">Kruthika S</div><div className="author-role">Crew at KathaKonnect Dance Event</div></div></div>
                 </div>
                 <div className="testimonial">
-                    <p className="testimonial-text">Finding reliable staff for our restaurant during festival seasons was always a nightmare until we discovered KrewsUp. Now we can scale our team up or down based on demand with qualified professionals.</p>
-                    <div className="testimonial-author"><div className="author-avatar">R</div><div><div className="author-name">Rajan Patel</div><div className="author-role">Restaurant Owner, Mumbai</div></div></div>
+                    <p className="testimonial-text">Explaining traditional Indian board games to customers was such a unique experience. The vibe was amazing, and the team energy was on point. KrewsUp made sure I was prepared and paid on time.</p>
+                    <div className="testimonial-author"><div className="author-avatar">T</div><div><div className="author-name">Thanusha</div><div className="author-role">Crew at RollTheDice Event</div></div></div>
+                </div>
+                <div className="testimonial">
+                    <p className="testimonial-text">Setting up the Urbanaut event gave me hands-on experience in managing a full-scale event. From assembling stalls to organizing spaces, it was intense but rewarding. KrewsUp made the whole gig experience effortless and professional.</p>
+                    <div className="testimonial-author"><div className="author-avatar">H</div><div><div className="author-name">Harsha Vardhan</div><div className="author-role">Crew at Urbanaut Event</div></div></div>
                 </div>
             </div>
         </section>
